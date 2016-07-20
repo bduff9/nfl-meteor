83c8ac2640ecb527f404b848da7676e606897865
Brian Duffey <bduff9@gmail.com>
2016-07-19 22:10:11 -0500

    Started Dashboard button for API call

--- imports/ui/pages/dashboard.jsx
+++ imports/ui/pages/dashboard.jsx
@@ -1,3 +1,19 @@
 import React from 'react';
 
-export const Dashboard = () => <h3>Dashboard</h3>;
+export const Dashboard = (props) => {
+  callAPI = (ev) => {
+    const currYear = new Date.getYear(),
+        week = 1,
+        url = `http://www03.myfantasyleague.com/${currYear}/export?TYPE=nflSchedule&L=&W=${week}&JSON=1`;
+    console.log(url);
+  };
+
+  return (
+    <div>
+      <h3>Dashboard</h3>
+      <button type="button" className="btn btn-primary" onClick={callAPI}>
+        Call API
+      </button>
+    </div>
+  );
+};

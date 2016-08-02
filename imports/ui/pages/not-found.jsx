7d6dc9aa31512b4b03ad836b5f92664bc43d49b6
Billy <balexander82@aol.com>
2016-08-01 22:20:21 -0500

    Added image to 404 page.

--- imports/ui/pages/not-found.jsx
+++ imports/ui/pages/not-found.jsx
@@ -6,20 +6,26 @@ export const NotFound = (props) => {
   return (
     <div className="flex-container">
       <Helmet title="Not Found" />
-      <div className="signin-form">
+      <div className="signin-form" style={{width:"600px"}}>
         <div className="row">
           <div className="text-xs-center col-xs-12">
-            <h1>404</h1>
+            <h1>What have you done?!</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="text-xs-center col-xs-12" style={{marginBottom:"25px"}}>
+            <img src="/404/rivers.jpg" alt="404 - Page Not Found" width="500px" />
           </div>
         </div>
         <div className="row">
           <div className="text-xs-center col-xs-12">
-            <h4>The page you're looking for cannot be found.</h4>
+            <h4>Something has gone wrong. It might be because of you. It might be because of us.
+            Either way, this is awkward.</h4>
           </div>
         </div>
         <div className="row">
           <div className="text-xs-center col-xs-12">
-            <Link to="/" activeClassName="active">Return to Home Page</Link>
+            <Link to="/" activeClassName="active">Please click here to get us both out of this situation</Link>
           </div>
         </div>
       </div>

633b538688e640ca157db165b2ea855d31a9e4f9
Brian Duffey <briand@mrc-productivity.com>
2016-08-25 17:31:43 -0500

    Fixed #24, updated #25 for testing on server

--- server/publications/nfllogs.js
+++ server/publications/nfllogs.js
@@ -43,7 +43,9 @@ Meteor.publishComposite('unreadChats', {
           fields: {
             '_id': 1,
             'action': 1,
-            'when': 1
+            'when': 1,
+            'message': 1,
+            'user_id': 1
           },
           sort: {
             when: -1

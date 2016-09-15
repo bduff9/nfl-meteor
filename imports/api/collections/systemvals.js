32f20cc6bfbb738cb6cd8211959b659e7576ba9f
Brian Duffey <briand@mrc-productivity.com>
2016-09-15 15:18:13 -0500

    Debugging scoreboard issue

--- imports/api/collections/systemvals.js
+++ imports/api/collections/systemvals.js
@@ -13,9 +13,16 @@ export const toggleScoreboard = new ValidatedMethod({
     if (!this.userId) throw new Meteor.Error('SystemVal.updateScoreboard.not-signed-in', 'You must be logged in to update system values');
     if (Meteor.isServer) {
       const connId = this.connection.id;
-      let systemVal = SystemVal.findOne();
-      systemVal.current_connections[connId].scoreboard_open = isOpen;
-      systemVal.save();
+      let systemVal = SystemVal.findOne(),
+          conn = systemVal.current_connections[connId];
+      if (conn) {
+        conn.scoreboard_open = isOpen;
+        systemVal.save();
+      } else {
+        console.log('Connection not found!');
+        console.log('connection id', connId);
+        console.log('connection', conn);
+      }
     }
   }
 });

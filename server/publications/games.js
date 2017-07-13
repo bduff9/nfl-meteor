d47defc12a88c4a0a27e741db65ad7d46b274ac7
Brian Duffey <bduff9@gmail.com>
2017-07-12 21:45:29 -0500

    Got past registration screen, added TODOs for new fields needed during registration for BA

--- server/publications/games.js
+++ server/publications/games.js
@@ -6,7 +6,7 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Game } from '../../imports/api/collections/games';
 
 Meteor.publish('nextGame', function () {
-	const nextGame = Game.find({ status: { $ne: 'C' }, game: { $ne: 0 }}, {
+	const nextGame = Game.find({ status: { $ne: 'C' }}, {
 		fields: {
 			'_id': 1,
 			'week': 1,

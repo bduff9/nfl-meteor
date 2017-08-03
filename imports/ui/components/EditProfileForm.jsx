e99e749eb370330c4d8ee0db54b30f31cc24b2b5
Brian Duffey <bduff9@gmail.com>
2017-08-02 21:59:34 -0500

    Converted edit profile to formik, still need to add new fields

--- imports/ui/components/EditProfileForm.jsx
+++ imports/ui/components/EditProfileForm.jsx
@@ -1,17 +1,24 @@
 'use strict';
 
 import React, { PropTypes } from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Formik } from 'formik';
 import Yup from 'yup';
 
 import { displayError } from '../../api/global';
+import { updateUser } from '../../api/collections/users';
+
+const _getInputColor = (error, touched, prefix) => {
+	if (!touched) return '';
+	if (error) return prefix + 'danger';
+	return prefix + 'success';
+};
 
 const EditProfileForm = ({
 	dirty,
 	errors,
+	hasFacebook,
+	hasGoogle,
 	isCreate,
 	isSubmitting,
 	touched,
@@ -19,65 +26,69 @@ const EditProfileForm = ({
 	handleBlur,
 	handleChange,
 	handleReset,
-	handleSubmit
+	handleSubmit,
+	linkFacebook,
+	linkGoogle
 }) => (
 	<form onSubmit={handleSubmit}>
-		<div className="row">
-			<div className="col-xs-12 form-group">
-				<label>Email</label>
+		<div className="row form-group">
+			<label className="col-xs-12 col-md-2 col-form-label">Email</label>
+			<div className="col-xs-12 col-md-10">
 				<p className="form-control-static">{values.email}</p>
 			</div>
 		</div>
-		<div className="row">
-			<div className="col-xs-12 col-md-6 form-group">
-				<label htmlFor="first_name">First Name</label>
-				<input type="text" className="form-control" name="firstName" placeholder="First Name" value={values.firstName} required onBlur={handleBlur} onChange={handleChange} />
-				{errors.firstName && touched.firstName && <div className="text-danger">{errors.firstName}</div>}
+		<div className="row form-group">
+			<label htmlFor="first_name" className="col-xs-12 col-md-2 col-form-label">Full Name</label>
+			<div className={`col-xs-12 col-md-5 ${_getInputColor(errors.first_name, touched.first_name, 'has-')}`}>
+				<input type="text" className={`form-control ${_getInputColor(errors.first_name, touched.first_name, 'form-control-')}`} name="first_name" placeholder="First Name" value={values.first_name} required onBlur={handleBlur} onChange={handleChange} />
+				{errors.first_name && touched.first_name && <div className="form-control-feedback">{errors.first_name}</div>}
 			</div>
-			<div className="col-xs-12 col-md-6 form-group">
-				<label htmlFor="last_name">Last Name</label>
-				<input type="text" className="form-control" name="lastName" placeholder="Last Name" value={values.lastName} required onBlur={handleBlur} onChange={handleChange} />
-			</div>
-		</div>
-		<div className="row">
-			<div className="col-xs-12 form-group">
-				<label htmlFor="team_name">Team Name (Optional)</label>
-				<input type="text" className="form-control" name="teamName" placeholder="Team Name (Optional)" value={values.teamName} onBlur={handleBlur} onChange={handleChange} />
+			<div className={`col-xs-12 col-md-5 ${_getInputColor(errors.last_name, touched.last_name, 'has-')}`}>
+				<input type="text" className={`form-control ${_getInputColor(errors.last_name, touched.last_name, 'form-control-')}`} name="last_name" placeholder="Last Name" value={values.last_name} required onBlur={handleBlur} onChange={handleChange} />
+				{errors.last_name && touched.last_name && <div className="form-control-feedback">{errors.last_name}</div>}
 			</div>
 		</div>
-		<div className="row">
-			<div className="col-xs-12 form-group">
-				<label htmlFor="referred_by">Referred By</label>
-				<input type="text" className="form-control" name="referredBy" placeholder="Referred By" value={values.referredBy} onBlur={handleBlur} onChange={handleChange} />
+		<div className={`row form-group ${_getInputColor(errors.team_name, touched.team_name, 'has-')}`}>
+			<label htmlFor="team_name" className="col-xs-12 col-md-2 col-form-label">Team Name (Optional)</label>
+			<div className="col-xs-12 col-md-10">
+				<input type="text" className={`form-control ${_getInputColor(errors.team_name, touched.team_name, 'form-control-')}`} name="team_name" placeholder="Team Name (Optional)" value={values.team_name} onBlur={handleBlur} onChange={handleChange} />
+				{errors.team_name && touched.team_name && <div className="form-control-feedback">{errors.team_name}</div>}
 			</div>
 		</div>
-		<div className="row">
-			<div className="col-xs-12 form-group text-xs-center save-wrapper">
-				<div className="row">
-					<div className="social-text col-xs-10 offset-xs-1 text-xs-center">
-						<strong>Note:</strong>
-					&nbsp;Linking your account makes logging in as simple as a single click
-					</div>
-				</div>
-				<div className="row form-group">
-					<div className="col-xs-12 col-md-6 text-xs-center text-md-right social-btns">
-						<button type="button" className="btn btn-primary btn-facebook" disabled={hasFacebook} onClick={oauthLink.bind(null, 'loginWithFacebook')}>
-							<i className="fa fa-facebook"></i> {hasFacebook ? 'Facebook Linked!' : 'Link Facebook'}
-						</button>
-					</div>
-					<div className="col-xs-12 col-md-6 text-xs-center text-md-left social-btns">
-						<button type="button" className="btn btn-danger btn-google" disabled={hasGoogle} onClick={oauthLink.bind(null, 'loginWithGoogle')}>
-							<i className="fa fa-google"></i> {hasGoogle ? 'Google Linked!' : 'Link Google'}
-						</button>
-					</div>
-				</div>
-				<div className="col-xs-12 save-btn">
-					<button type="submit" className="btn btn-primary" disabled={isSubmitting}>
-						<i className="fa fa-fw fa-save"></i>
-						{isCreate ? 'Finish Registration' : 'Save Changes'}
-						{isSubmitting ? <i className="fa fa-fw fa-spinner fa-pulse" /> : null}
-					</button>
-				</div>
+		<div className={`row form-group ${_getInputColor(errors.referred_by, touched.referred_by, 'has-')}`}>
+			<label htmlFor="referred_by" className="col-xs-12 col-md-2 col-form-label">Referred By</label>
+			<div className="col-xs-12 col-md-10">
+				<input type="text" className={`form-control ${_getInputColor(errors.referred_by, touched.referred_by, 'form-control-')}`} name="referred_by" placeholder="Referred By" value={values.referred_by} onBlur={handleBlur} onChange={handleChange} />
+				{errors.referred_by && touched.referred_by && <div className="form-control-feedback">{errors.referred_by}</div>}
+			</div>
+		</div>
+		<div className="row form-group">
+			<div className="col-xs-10 offset-xs-1 text-xs-center">
+				<p className="form-text text-muted">
+					<strong>Note:</strong> &nbsp;Linking your account makes logging in as simple as a single click
+				</p>
+			</div>
+			<div className="col-xs-12 col-md-6 text-xs-center text-md-right social-btns">
+				<button type="button" className="btn btn-primary btn-facebook" disabled={hasFacebook} onClick={linkFacebook}>
+					<i className="fa fa-facebook"></i> {hasFacebook ? 'Facebook Linked!' : 'Link Facebook'}
+				</button>
+			</div>
+			<div className="col-xs-12 col-md-6 text-xs-center text-md-left social-btns">
+				<button type="button" className="btn btn-danger btn-google" disabled={hasGoogle} onClick={linkGoogle}>
+					<i className="fa fa-google"></i> {hasGoogle ? 'Google Linked!' : 'Link Google'}
+				</button>
+			</div>
+		</div>
+		<div className="row form-group">
+			<div className="col-xs-12">&nbsp;</div>
+		</div>
+		<div className="row form-group">
+			<div className="col-xs-12 text-xs-center">
+				<button type="submit" className="btn btn-primary" disabled={isSubmitting}>
+					<i className="fa fa-fw fa-save"></i>
+					{isCreate ? 'Finish Registration' : 'Save Changes'}
+					{isSubmitting ? <i className="fa fa-fw fa-spinner fa-pulse" /> : null}
+				</button>
 			</div>
 		</div>
 	</form>
@@ -87,28 +98,51 @@ EditProfileForm.propTypes = {
 	dirty: PropTypes.bool.isRequired,
 	error: PropTypes.object,
 	errors: PropTypes.object.isRequired,
+	hasFacebook: PropTypes.bool.isRequired,
+	hasGoogle: PropTypes.bool.isRequired,
 	isCreate: PropTypes.bool.isRequired,
 	isSubmitting: PropTypes.bool.isRequired,
+	router: PropTypes.object.isRequired,
 	touched: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired,
 	values: PropTypes.object.isRequired,
 	handleBlur: PropTypes.func.isRequired,
 	handleChange: PropTypes.func.isRequired,
 	handleReset: PropTypes.func.isRequired,
-	handleSubmit: PropTypes.func.isRequired
+	handleSubmit: PropTypes.func.isRequired,
+	linkFacebook: PropTypes.func.isRequired,
+	linkGoogle: PropTypes.func.isRequired
 };
 
 export default Formik({
 
-	mapPropsToValues: props => ({
-		//TODO:
-	}),
+	mapPropsToValues: props => props.user,
 
 	validationSchema: props => Yup.object().shape({
-		email: Yup.string().email('Please enter a valid email').required('Please enter your email address')
-		//TODO:
+		first_name: Yup.string().min(2, 'Please enter your first name').required('Please enter your first name'),
+		last_name: Yup.string().min(2, 'Please enter your surname').required('Please enter your surname'),
+		team_name: Yup.string(),
+		referred_by: Yup.string().matches(/\s/, 'Please input the full name of the person that invited you').required('Please input the full name of the person that invited you')
 	}),
 
 	handleSubmit: (values, { props, setErrors, setSubmitting }) => {
-		//TODO:
+		const { first_name, survivor, last_name, payment_account, payment_type, referred_by, team_name } = values,
+				{ isCreate, router } = props,
+				done_registering = true;
+		try {
+			updateUser.call({ done_registering, first_name, last_name, payment_account, payment_type, referred_by, survivor, team_name });
+			if (isCreate) {
+				Bert.alert(`Thanks for registering, ${first_name}`, 'success');
+				router.push('/');
+			} else {
+				Bert.alert({
+					message: 'Profile saved!',
+					type: 'success',
+					icon: 'fa-save'
+				});
+			}
+		} catch(err) {
+			displayError(err);
+		}
 	}
 })(EditProfileForm);
--- imports/ui/pages/EditProfile.jsx
+++ imports/ui/pages/EditProfile.jsx
@@ -3,51 +3,25 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
-import $ from 'jquery';
 import 'jquery-validation';
 import Helmet from 'react-helmet';
 
-import { updateUser } from '../../api/collections/users';
 import { displayError } from '../../api/global';
+import EditProfileForm from '../components/EditProfileForm';
 
 export default class EditProfile extends Component {
 
 	constructor (props) {
-		const { location } = props,
-				isCreate = location.pathname.indexOf('create') > -1,
-				isEdit = !isCreate,
-				user = Meteor.user();
+		const user = Meteor.user();
 		super();
 		this.state = {
-			firstName: user.first_name,
 			hasFacebook: !!user.services && !!user.services.facebook,
 			hasGoogle: !!user.services && !!user.services.google,
-			hasSurvivor: false, //TODO: add this to page
-			isCreate,
-			isEdit,
-			lastName: user.last_name,
-			paymentAccount: '', //TODO: add this to page
-			paymentType: 'Cash', //TODO: add this to page
-			referredBy: user.referred_by,
-			showReferredBy: false,
-			teamName: user.team_name
+			isCreate: props.location.pathname.indexOf('create') > -1,
+			user
 		};
-		this._handleChanges = this._handleChanges.bind(this);
-		this._handleReferredBy = this._handleReferredBy.bind(this);
-		this._updateUser = this._updateUser.bind(this);
-		this._validate = this._validate.bind(this);
 	}
 
-	componentDidMount () {
-		this._validate();
-	}
-	_handleChanges (ev) {
-		const el = ev.currentTarget;
-		this.setState({ [el.id]: el.value });
-	}
-	_handleReferredBy (showReferredBy, ev) {
-		this.setState({ showReferredBy, referredBy: (!showReferredBy ? 'RETURNING' : '') });
-	}
 	_oauthLink (service, ev) {
 		const options = {
 			requestPermissions: ['email']
@@ -63,68 +37,10 @@ export default class EditProfile extends Component {
 			}
 		});
 	}
-	_submitForm (ev) {
-		ev.preventDefault();
-	}
-	_updateUser () {
-		const { firstName, hasSurvivor, isCreate, lastName, paymentAccount, paymentType, referredBy, teamName } = this.state,
-				{ router } = this.context,
-				DONE_REGISTERING = true;
-		try {
-			updateUser.call({ done_registering: DONE_REGISTERING, first_name: firstName, last_name: lastName, payment_account: paymentAccount, payment_type: paymentType, referred_by: referredBy, survivor: hasSurvivor, team_name: teamName });
-			if (isCreate) {
-				Bert.alert(`Thanks for registering, ${firstName}`, 'success');
-				router.push('/');
-			} else {
-				Bert.alert({
-					message: 'Profile saved!',
-					type: 'success',
-					icon: 'fa-save'
-				});
-			}
-		} catch(err) {
-			displayError(err);
-		}
-	}
-	_validate () {
-		const that = this;
-		$(this.userFormRef).validate({
-			errorLabelContainer: '#error-messages',
-			wrapper: 'li',
-			submitHandler () {
-				that._updateUser();
-			},
-			rules: {
-				email: {
-					required: true,
-					email: true
-				},
-				firstName: {
-					required: true
-				},
-				lastName: {
-					required: true
-				},
-				was_referred: {
-					required: true
-				},
-				referredBy: {
-					required: true
-				}
-			},
-			messages: {
-				email: 'Please enter a valid email address',
-				firstName: 'Please enter your first name',
-				lastName: 'Please enter your surname',
-				was_referred: 'Please select whether you are new or a returning player',
-				referredBy: 'Please indicate who you were referred by'
-			}
-		});
-	}
 
 	render () {
-		const { firstName, hasFacebook, hasGoogle, isCreate, lastName, referredBy, showReferredBy, teamName } = this.state,
-				user = Meteor.user();
+		const { hasFacebook, hasGoogle, isCreate, user } = this.state,
+				{ router } = this.context;
 		return (
 			<div className="container-fluid edit-profile-wrapper">
 				<div className="row">
@@ -136,89 +52,7 @@ export default class EditProfile extends Component {
 							</div>
 						</div>
 						<div className="edit-profile">
-							<ul id="error-messages"></ul>
-							<form ref={form => { this.userFormRef = form; }} onSubmit={this._submitForm}>
-								<div className="row">
-									<div className="col-xs-12 form-group floating-label-form-group floating-label-form-group-with-value">
-										<label>Email</label>
-										<p className="form-control-static">{user.email}</p>
-									</div>
-								</div>
-								<div className="row">
-									<div className={'col-xs-12 col-md-6 form-group floating-label-form-group' + (user.first_name ? ' floating-label-form-group-with-value' : '')}>
-										<label htmlFor="first_name">First Name</label>
-										<input type="text" className="form-control" id="firstName" name="firstName" placeholder="First Name" value={firstName} required={true} onChange={this._handleChanges} />
-									</div>
-									<div className={'col-xs-12 col-md-6 form-group floating-label-form-group' + (user.last_name ? ' floating-label-form-group-with-value' : '')}>
-										<label htmlFor="last_name">Last Name</label>
-										<input type="text" className="form-control" id="lastName" name="lastName" placeholder="Last Name" value={lastName} required={true} onChange={this._handleChanges} />
-									</div>
-								</div>
-								<div className="row">
-									<div className={'col-xs-12 form-group floating-label-form-group' + (user.team_name ? ' floating-label-form-group-with-value' : '')}>
-										<label htmlFor="team_name">Team Name (Optional)</label>
-										<input type="text" className="form-control" id="teamName" placeholder="Team Name (Optional)" value={teamName} onChange={this._handleChanges} />
-									</div>
-								</div>
-								{isCreate ?
-									<div className="row prev-play">
-										<div className="col-xs-12 form-group">
-											<div className="radio">
-												<label htmlFor="referred_byN">
-													<input type="radio" id="referred_byN" name="was_referred" onClick={this._handleReferredBy.bind(null, false)} />
-													&nbsp;&nbsp;I have played previously
-												</label>
-											</div>
-											<div className="radio">
-												<label htmlFor="referred_byY">
-													<input type="radio" id="referred_byY" name="was_referred" onClick={this._handleReferredBy.bind(null, true)} />
-														&nbsp;&nbsp;I am new and was referred by:
-												</label>
-											</div>
-										</div>
-									</div>
-									:
-									null
-								}
-								{showReferredBy ?
-									<div className="row">
-										<div className={'col-xs-12 form-group floating-label-form-group' + (user.referred_by ? ' floating-label-form-group-with-value' : '')}>
-											<label htmlFor="referred_by">Name of Referrer</label>
-											<input type="text" className="form-control" id="referredBy" name="referredBy" placeholder="Referred By" defaultValue={referredBy} onChange={this._handleChanges} />
-										</div>
-									</div>
-									:
-									null
-								}
-								<div className="row">
-									<div className="col-xs-12 form-group text-xs-center save-wrapper">
-										<div className="row">
-											<div className="social-text col-xs-10 offset-xs-1 text-xs-center">
-												<strong>Note:</strong>
-											&nbsp;Linking your account makes logging in as simple as a single click
-											</div>
-										</div>
-										<div className="row form-group floating-label-form-group">
-											<div className="col-xs-12 col-md-6 text-xs-center text-md-right social-btns">
-												<button type="button" className="btn btn-primary btn-facebook" disabled={hasFacebook} onClick={this._oauthLink.bind(null, 'loginWithFacebook')}>
-													<i className="fa fa-facebook"></i> {hasFacebook ? 'Facebook Linked!' : 'Link Facebook'}
-												</button>
-											</div>
-											<div className="col-xs-12 col-md-6 text-xs-center text-md-left social-btns">
-												<button type="button" className="btn btn-danger btn-google" disabled={hasGoogle} onClick={this._oauthLink.bind(null, 'loginWithGoogle')}>
-													<i className="fa fa-google"></i> {hasGoogle ? 'Google Linked!' : 'Link Google'}
-												</button>
-											</div>
-										</div>
-										<div className="col-xs-12 save-btn">
-											<button type="submit" className="btn btn-primary">
-												<i className="fa fa-fw fa-save"></i>
-												{isCreate ? 'Finish Registration' : 'Save Changes'}
-											</button>
-										</div>
-									</div>
-								</div>
-							</form>
+							<EditProfileForm hasFacebook={hasFacebook} hasGoogle={hasGoogle} isCreate={isCreate} router={router} user={user} linkFacebook={this._oauthLink.bind(null, 'loginWithFacebook')} linkGoogle={this._oauthLink.bind(null, 'loginWithGoogle')} />
 						</div>
 					</div>
 				</div>

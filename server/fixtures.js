ee7c7938bf3ac12f72d6a5683bc6abb5404a2f10
Brian Duffey <briand@mrc-productivity.com>
2017-09-20 09:02:28 -0500

    Removed manual code now that it has been run

--- server/fixtures.js
+++ server/fixtures.js
@@ -9,60 +9,6 @@ import { teamsExistSync } from '../imports/api/collections/teams';
 import { initScheduleSync } from './collections/games';
 import { initTeamsSync } from './collections/teams';
 
-//TODO: remove manual code after it is run
-import { Pick } from '../imports/api/collections/picks';
-import { addPick } from './collections/picks';
-import { getTiebreakerFromServer } from './collections/tiebreakers';
-import { User } from '../imports/api/collections/users';
-import { getLowestScore } from './collections/users';
-const brett = User.findOne({ first_name: 'Brett', last_name: 'Verplank' }),
-		kyle = User.findOne({ first_name: 'Kyle', last_name: 'Samp' }),
-		lowestUser = getLowestScore.call({ current_user_ids: [brett._id, kyle._id], league: brett.leagues[0], week: 2 });
-const lowestUserPicks = Pick.find({ user_id: lowestUser._id, week: 1 }).fetch();
-Pick.remove({ week: 1, user_id: brett._id });
-Pick.remove({ week: 1, user_id: kyle._id });
-lowestUserPicks.forEach(pick => {
-	const { week, league, game_id, game, pick_id, pick_short, points, winner_id, winner_short } = pick;
-	let pickToAdd = {
-		user_id: brett._id,
-		week,
-		league,
-		game_id,
-		game,
-		pick_id,
-		pick_short,
-		points,
-		winner_id,
-		winner_short
-	};
-	addPick.call({ pick: pickToAdd });
-	pickToAdd.user_id = kyle._id;
-	addPick.call({ pick: pickToAdd });
-	const lowestTB = getTiebreakerFromServer.call({ league, user_id: lowestUser._id, week: 1 }),
-			{ last_score, last_score_act, points_earned, games_correct, place_in_week } = lowestTB;
-	let userTB = getTiebreakerFromServer.call({ league, user_id: brett._id, week: pick.week });
-	lowestTB.tied_flag = true;
-	lowestTB.save();
-	userTB.last_score = last_score;
-	userTB.last_score_act = last_score_act;
-	userTB.points_earned = points_earned;
-	userTB.games_correct = games_correct;
-	userTB.place_in_week = place_in_week;
-	userTB.tied_flag = true;
-	userTB.submitted = true;
-	userTB.save();
-	userTB = getTiebreakerFromServer.call({ league, user_id: kyle._id, week: pick.week });
-	userTB.last_score = last_score;
-	userTB.last_score_act = last_score_act;
-	userTB.points_earned = points_earned;
-	userTB.games_correct = games_correct;
-	userTB.place_in_week = place_in_week;
-	userTB.tied_flag = true;
-	userTB.submitted = true;
-	userTB.save();
-});
-//TODO: end section to remove
-
 if (!teamsExistSync()) {
 	console.log('Begin populating teams...');
 	initTeamsSync();
